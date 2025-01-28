da3930df762da675deb992373a5f8b84e4471ddc
Manuel0311Nan <manuzz3112@gmail.com>
2025-01-28 20:59:23 +0100

    conexion with api books

--- src/app/pages/book-gallery/book-gallery.component.ts
+++ src/app/pages/book-gallery/book-gallery.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { BooksApiService } from '../../core/services/books-api/books-api.service';
 @Component({
   selector: 'app-book-gallery',
   imports: [],
@@ -7,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrl: './book-gallery.component.scss'
 })
 export class BookGalleryComponent {
+  books: any[] = [];
 
+  constructor(private booksApiService: BooksApiService) {
+  }
+  ngOnInit() {
+    this.booksApiService.getBooks().subscribe({
+      next: (response) => (this.books = response),
+      error: (err) => console.error('Error:', err),
+    });
+    console.log(this.books);
+  }
 }
