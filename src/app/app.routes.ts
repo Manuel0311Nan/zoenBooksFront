758489d0931b5281d83f834e25e3970d85146879
Manuel0311Nan <manuzz3112@gmail.com>
2025-01-28 20:30:39 +0100

    add project routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { HomeComponent } from './pages/home/home.component';
+import { BookGalleryComponent } from './pages/book-gallery/book-gallery.component';
+import { BookDetailComponent } from './pages/book-detail/book-detail.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { YggdrasilComponent } from './pages/yggdrasil/yggdrasil.component';
+// Define las rutas principales de tu aplicación
+export const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' }, 
+  { path: '', component: HomeComponent },
+  { path: 'book/:id', component: BookDetailComponent },
+  { path: 'gallery', component: BookGalleryComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'yggdrasil', component: YggdrasilComponent },
+  { path: '**', redirectTo: '' } 
+];
