ad6c1551124a779088673a2af3784ff9a765ef3d
Manuel0311Nan <manuzz3112@gmail.com>
2025-01-30 13:49:46 +0100

    create gallery stucture

--- src/app/pages/book-gallery/book-gallery.component.ts
+++ src/app/pages/book-gallery/book-gallery.component.ts
@@ -1,21 +1,46 @@
 import { Component } from '@angular/core';
 import { BooksApiService } from '../../core/services/books-api/books-api.service';
+import { timer } from 'rxjs';
+import { NgIf } from '@angular/common';
+import { Skeleton } from 'primeng/skeleton';
 @Component({
   selector: 'app-book-gallery',
-  imports: [],
+  imports: [NgIf, Skeleton],
   templateUrl: './book-gallery.component.html',
   styleUrl: './book-gallery.component.scss'
 })
 export class BookGalleryComponent {
   books: any[] = [];
+  placeholderBooks = [
+    { title: 'Libro Misterioso', description: 'Una gran historia...', image: 'assets/placeholder-book.jpg' },
+    { title: 'Aventura en Midgard', description: 'Descubre tierras míticas...', image: 'assets/placeholder-book.jpg' },
+    { title: 'La Torre de los Sabios', description: 'Una historia mágica...', image: 'assets/placeholder-book.jpg' }
+  ];
+  isLoading = true;
+  timeoutReached = false;
 
   constructor(private booksApiService: BooksApiService) {
   }
   ngOnInit() {
-    this.booksApiService.getBooks().subscribe({
-      next: (response) => (this.books = response),
-      error: (err) => console.error('Error:', err),
+
+    const timeout = timer(60000).subscribe(() => {
+      if (this.isLoading) {
+        this.timeoutReached = true;
+        this.isLoading = false; // Detenemos el skeleton
+      }
     });
-    console.log(this.books);
+
+    this.booksApiService.getBooks().subscribe({
+      next: (data) => {
+        this.books = data;
+        this.isLoading = false; // Se detiene el skeleton si llegan los datos
+        timeout.unsubscribe(); // Cancela el timeout si la API responde antes del minuto
+      },
+      error: () => {
+        this.isLoading = false;
+        this.timeoutReached = true; // Si hay error, se muestran los elementos precargados
+        timeout.unsubscribe();
+      }
+    });
   }
 }
