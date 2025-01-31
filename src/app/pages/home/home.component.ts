7bb02fdb9e3867a4b62ecbe02505e9532baafee1
Manuel0311Nan <manuzz3112@gmail.com>
2025-01-31 09:44:27 +0100

    new section home: crows

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
-
+import { RouterModule } from '@angular/router';
 @Component({
   selector: 'app-home',
-  imports: [],
+  standalone: true,
+  imports: [RouterModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+
+  crowList = [
+    { title: 'Negro con detalles azules', description: 'Prefieres resolver acertijos', image: 'assets/placeholder-book.jpg', id: 'Azul' },
+    { title: 'Negro con detalles verdes', description: 'A veces un combate es la mejor solución.', image: 'assets/placeholder-book.jpg', id: 'Verde'},
+    { title: 'Negro con detalles púrpuras', description: 'Los magos hacemos las cosas a nuestra manera', image: 'assets/placeholder-book.jpg', id:'Purpura' }
+  ];
 
 }
